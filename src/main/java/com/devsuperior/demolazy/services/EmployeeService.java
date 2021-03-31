99fcda911800c4d245c484fd64d94fb898920284
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2021-03-31 16:26:44 -0300

    Primeiro commit

---
+++ src/main/java/com/devsuperior/demolazy/services/EmployeeService.java
@@ -0,0 +1,33 @@
+package com.devsuperior.demolazy.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import com.devsuperior.demolazy.dto.EmployeeDTO;
+import com.devsuperior.demolazy.entities.Employee;
+import com.devsuperior.demolazy.repositories.EmployeeRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+
+	@Autowired
+	private EmployeeRepository repository;
+	
+	public EmployeeDTO findById(Long id) {
+		Optional<Employee> result = repository.findById(id);
+		return new EmployeeDTO(result.get());
+	}
+
+	public List<EmployeeDTO> findAll() {
+		List<Employee> list = repository.findAll();
+		return list.stream().map(x -> new EmployeeDTO(x)).collect(Collectors.toList());
+	}
+
+
+}
+
+
